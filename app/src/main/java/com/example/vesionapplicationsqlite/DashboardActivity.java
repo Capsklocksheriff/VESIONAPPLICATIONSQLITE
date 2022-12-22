1e99cf100ece7880a9dff83ac9b27fd643dff6e9
Capslocksheriff <marvinandrei88@gmail.com>
2022-12-22 19:34:30 +0800

    git commit

---
+++ app/src/main/java/com/example/vesionapplicationsqlite/DashboardActivity.java
@@ -0,0 +1,53 @@
+package com.example.vesionapplicationsqlite;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    String EmailHolder;
+    TextView Email;
+    Button LogOUT ;
+    Button Location;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        Email = (TextView)findViewById(R.id.textView1);
+        LogOUT = (Button)findViewById(R.id.button1);
+        Location = (Button)findViewById(R.id.button2);
+
+        Intent intent = getIntent();
+
+        // Receiving User Email Send By MainActivity.
+        EmailHolder = intent.getStringExtra(MainActivity.UserEmail);
+
+        // Setting up received email to TextView.
+        Email.setText(Email.getText().toString()+ EmailHolder);
+
+        // Adding click listener to Log Out button.
+        LogOUT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Finishing current DashBoard activity on button click.
+                finish();
+
+                Toast.makeText(DashboardActivity.this,"Log Out Successfull", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+}
